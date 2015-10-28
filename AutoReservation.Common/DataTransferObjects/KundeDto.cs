c266c3453f50cceea5a4afcba291a5f42ce818cd
oxylius <noah.hendrikx@gmail.com>
2015-10-28 08:48:27 +0100

    Defined DTO Properties

--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -1,51 +1,112 @@
 ﻿using AutoReservation.Common.Extensions;
 using AutoReservation.Common.DataTransferObjects.Core;
+using System.Text;
+using System;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class KundeDto //: DtoBase<KundeDto>
+    public class KundeDto : DtoBase<KundeDto>
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if(id == value)
+                {
+                    return;
+                }
+                id = value;
+                this.OnPropertyChanged(p => p.Id);
+            }
+        }
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(Nachname))
-        //    {
-        //        error.AppendLine("- Nachname ist nicht gesetzt.");
-        //    }
-        //    if (string.IsNullOrEmpty(Vorname))
-        //    {
-        //        error.AppendLine("- Vorname ist nicht gesetzt.");
-        //    }
-        //    if (Geburtsdatum == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
-        //    }
+        private string nachname;
+        public string Nachname
+        {
+            get { return nachname; }
+            set
+            {
+                if (nachname.Equals(value))
+                {
+                    return;
+                }
+                nachname = value;
+                this.OnPropertyChanged(p => p.Nachname);
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        private string vorname;
+        public string Vorname
+        {
+            get { return vorname; }
+            set
+            {
+                if (vorname.Equals(value))
+                {
+                    return;
+                }
+                vorname = value;
+                this.OnPropertyChanged(p => p.Vorname);
+            }
+        }
 
-        //    return error.ToString();
-        //}
+        private DateTime geburtsdatum;
+        public DateTime Geburtsdatum
+        {
+            get { return geburtsdatum; }
+            set
+            {
+                if (geburtsdatum.Equals(value))
+                {
+                    return;
+                }
+                geburtsdatum = value;
+                this.OnPropertyChanged(p => p.Geburtsdatum);
+            }
+        }
 
-        //public override KundeDto Clone()
-        //{
-        //    return new KundeDto
-        //    {
-        //        Id = Id,
-        //        Nachname = Nachname,
-        //        Vorname = Vorname,
-        //        Geburtsdatum = Geburtsdatum
-        //    };
-        //}
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(Nachname))
+            {
+                error.AppendLine("- Nachname ist nicht gesetzt.");
+            }
+            if (string.IsNullOrEmpty(Vorname))
+            {
+                error.AppendLine("- Vorname ist nicht gesetzt.");
+            }
+            if (Geburtsdatum == DateTime.MinValue)
+            {
+                error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
+            }
 
-        //public override string ToString()
-        //{
-        //    return string.Format(
-        //        "{0}; {1}; {2}; {3}",
-        //        Id,
-        //        Nachname,
-        //        Vorname,
-        //        Geburtsdatum);
-        //}
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override KundeDto Clone()
+        {
+            return new KundeDto
+            {
+                Id = Id,
+                Nachname = Nachname,
+                Vorname = Vorname,
+                Geburtsdatum = Geburtsdatum
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}; {1}; {2}; {3}",
+                Id,
+                Nachname,
+                Vorname,
+                Geburtsdatum);
+        }
     }
 }
